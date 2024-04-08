ff5ea7dd9239e4ece795616e469bb418a7dbca25
Baskaran S <131956273+BASKARANS2001@users.noreply.github.com>
2024-04-08 15:15:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var typed = new Typed(".text", {
+  strings:[ "web Developer" ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop:true
+});
+
+
+
+const darkModeToggle = document.getElementById('darkModeToggle');
+const body = document.body;
+const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+if (isDarkMode) {
+    body.classList.add('dark-mode');
+    darkModeToggle.checked = true;
+}
+darkModeToggle.addEventListener('change', () => {
+    if (darkModeToggle.checked) {
+        body.classList.add('dark-mode');
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        body.classList.remove('dark-mode');
+        localStorage.setItem('darkMode', 'disabled');
+    }
+});
